34644b87a5eaa18bc183cb64271fc094c1787f24
Oktapra Amtono <oktapra.amtono@gmail.com>
2021-02-06 06:40:00 +0700

    fs/exfat: fixed after merge linux 4.4.256
    Signed-off-by: Oktapra Amtono <oktapra.amtono@gmail.com>

--- fs/exfat/super.c
+++ fs/exfat/super.c
@@ -2145,7 +2145,8 @@ static const struct inode_operations exfat_symlink_inode_operations = {
 #if LINUX_VERSION_CODE < KERNEL_VERSION(4, 10, 0)
 	.readlink    = generic_readlink,
 #endif
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 5, 0) && \
+      LINUX_VERSION_CODE < KERNEL_VERSION(4, 4, 256)
 	.get_link = exfat_follow_link,
 #else /* LINUX_VERSION_CODE < KERNEL_VERSION(4, 5, 0) */
 	.follow_link = exfat_follow_link,
